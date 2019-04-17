b4d54acd254ecb64bc074332bae0af85b8993467
alemembaye <45495838+alemembaye@users.noreply.github.com>
2019-04-16 23:43:45 -0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,7 +60,7 @@
             }
 
             .m-b-md {
-                margin-bottom: 30px;
+                margin-bottom: 40px;
             }
         </style>
     </head>
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   alem embaye
                 </div>
 
                 <div class="links">
